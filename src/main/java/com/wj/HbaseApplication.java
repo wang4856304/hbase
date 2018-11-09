e1245094b5ee509ac05149aa8651453db4e0f7b2
ReacherWang <reacherwang@qq.com>
2018-11-09 18:00:51 +0800

    hbase

---
+++ src/main/java/com/wj/HbaseApplication.java
@@ -0,0 +1,18 @@
+package com.wj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author wangJun
+ * @Description //TODO
+ * @Date ${date} ${time}
+ **/
+
+@SpringBootApplication
+public class HbaseApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(HbaseApplication.class, args);
+    }
+}
---
+++ src/main/java/com/wj/config/HBaseConfig.java
@@ -0,0 +1,14 @@
+package com.wj.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author wangJun
+ * @Description //TODO
+ * @Date ${date} ${time}
+ **/
+
+@Configuration
+public class HBaseConfig {
+
+}
